4bfddb333c6e6de7c8074a2525e2649ac6f58f12
Yasar <yaltamiranoa@outlook.com>
2020-09-30 16:55:03 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://droid:w17CuFBCT5LbUDDp@cluster0.iiwx6.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
